78d4b19bae43dec0a09f204e9a1d482f8cfa3fdd
declannoble <declanjnoble@hotmail.com>
2022-04-26 02:27:03 -0700

    style: betty

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,23 +7,18 @@
  */
 int check_cycle(listint_t *list)
 {
-  listint_t *current = list, *tmp = list;
+	listint_t *current = list, *tmp = list;
 
-  if (list == NULL || list->next == NULL)
-    return (0);
+	if (list == NULL || list->next == NULL)
+		return (0);
 
-  while (current->next != NULL)
-    {
-      if (current == tmp)
-	return (1);
+	while (current->next != NULL)
+	{
+		if (current == tmp)
+			return (1);
 
-      if (current->next == NULL)
+		if (current->next == NULL)
+			return (0);
+	}
 	return (0);
-
-    }
-  return (0);
 }
-      
-      
-
-  
