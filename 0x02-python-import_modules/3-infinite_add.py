7366f18f95e9700529ddcb5beea0df54f2d27a3a
declannoble <declanjnoble@hotmail.com>
2022-04-27 21:24:10 -0700

    build: fixed output issue

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-
 import sys
+
 if __name__ == "__main__":
 
-    result = 0
     ac = len(sys.argv)
+    result = 0
 
     for i in range(1, ac):
-        result = result + int(sys.argv[i])
+        result += int(sys.argv[i])
 
-        print(result)
+    print(result)
