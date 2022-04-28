7a2b5e375b7363c57979add2e3fcf5a5706e3ad2
declannoble <declanjnoble@hotmail.com>
2022-04-28 02:32:32 -0700

    perf: fixed if condition + vars

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    import sys
     import hidden_4
 
     result = dir(hidden_4)
     for name in result:
-        if name[0] != "_":
-            print(result)
+        if name[0:2] == '__':
+            continue
+        print(name)
