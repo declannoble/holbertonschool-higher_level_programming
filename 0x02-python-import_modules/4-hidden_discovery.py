5bc74598160226d84b84aa3193d2b90918a14afd
declannoble <declanjnoble@hotmail.com>
2022-04-28 02:26:29 -0700

    build: initial commit

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    import hidden_4
+
+    result = dir(hidden_4)
+    for name in result:
+        if name[0] != "-":
+            print(result)
