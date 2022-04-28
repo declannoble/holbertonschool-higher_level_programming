c1520994fdd16480b170f2df3102272e83ce9565
declannoble <declanjnoble@hotmail.com>
2022-04-27 21:15:05 -0700

    build: initial commit

---
+++ 0x02-python-import_modules/3-inifinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import sys
+if __name__ == "__main__":
+
+    ac = len(sys.argv)
+
+    for i in range(1, ac):
+        result = result + int(sys.argv[i])
+
+        print(result)
