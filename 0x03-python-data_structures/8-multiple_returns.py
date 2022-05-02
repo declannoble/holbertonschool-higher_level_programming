10073f6b07b98bcbfc648605b97480fba941445e
declannoble <declanjnoble@hotmail.com>
2022-05-01 22:49:29 -0700

    build: initialized variable

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
+    senlen = len(sentence)
     if sentence == "":
         return(0,None)
-    return(len(sentence), sentence[0])
+    return(senlen), sentence[0])
