8628c34d90158da3d23662296c6f990da73f079d
declannoble <declanjnoble@hotmail.com>
2022-05-01 23:08:02 -0700

    build: retyped

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,8 +5,7 @@ def divisible_by_2(my_list=[]):
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
             newlist.append(True)
-
         else:
             newlist.append(False)
 
-            return(newlist)
+    return (newlist)
