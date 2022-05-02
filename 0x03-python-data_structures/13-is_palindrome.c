c1f046d1eb664f462abe99b822c9fc5e9d8c2d76
declannoble <declanjnoble@hotmail.com>
2022-05-02 04:03:36 -0700

    build: initial commit

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * is_palindrome- checks if a linked list is a palindrome
+ * @head: pointer to head of list
+ * Return: 0 if not, and 1 if so.
+ */
+
+int is_palindrome(listint_t **head);
+{
+	if (*head == NULL || *head->next == NULL)
+		return (1);
+	while (*head != NULL)
+	{
+		dosomething;
+	}
+}
