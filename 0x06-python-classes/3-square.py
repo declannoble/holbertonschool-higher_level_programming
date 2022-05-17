882c700c587a0c492f11c200f73807c84a4e2f6f
declannoble <declanjnoble@hotmail.com>
2022-05-17 00:21:31 -0700

    docs

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-""" Defining a square class with a private instance attribute 'size' """
+"""A class Square with private instance attribute size
+    Instantiation with size
+    raise errors if size is not an int or less than 0
+    Public instance method area which returns the square area"""
 
 
 class Square:
+    """A class Square"""
     def __init__(self, size=0):
-        """instantiating data"""
-        if type(size) is not int:
+        """Raise errors if size is not an int or less than 0"""
+        if isinstance(size, int) is not True:
             raise TypeError("size must be an integer")
-        else:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                """instantiate with size"""
-                self.__size = size
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        """Instantiate with size"""
+        self.__size = size
 
+    """Returns the square area"""
     def area(self):
-        """Returns area of the square"""
-        return (self.__size) ** 2
+        return (self.__size**2)
