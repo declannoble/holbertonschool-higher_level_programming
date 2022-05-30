40f1237a43da22bbf518c2e978d9b0281b3a063d
declannoble <declanjnoble@hotmail.com>
2022-05-30 01:06:54 -0700

    refactor: changed function

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """returns True or False based on inheritance"""
-    if isinstance(obj, a_class) is True and type(obj) != a_class:
+    if issubclass(obj, a_class) is True and type(obj) != a_class:
         return True
 
     else:
