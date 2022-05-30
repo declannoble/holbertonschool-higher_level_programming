899fbcdc1fecd9c5131e316ce1ee616abf5e8938
declannoble <declanjnoble@hotmail.com>
2022-05-29 17:41:25 -0700

    build: initial commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" module creating a class BaseGeometry """
+
+
+class BaseGeometry:
+
+    def area(self):
+        """Public instance method for area"""
+        raise Exception("area() is not implemented")
