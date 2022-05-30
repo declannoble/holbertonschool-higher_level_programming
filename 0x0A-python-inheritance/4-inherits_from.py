432644121b4f20a03bc51688848a3eef1f7c4342
declannoble <declanjnoble@hotmail.com>
2022-05-30 01:03:46 -0700

    build: initial commit

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""inherits_from module """
+
+
+def inherits_from(obj, a_class):
+    """returns True or False based on inheritance"""
+    if isinstance(obj, a_class) is True and type(obj) != a_class:
+        return True
+
+    else:
+        return False
