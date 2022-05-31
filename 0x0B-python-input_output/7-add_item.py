283273a5616c63877b773186603ff7a4e8cbbb64
declannoble <declanjnoble@hotmail.com>
2022-05-31 01:20:50 -0700

    added line

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,6 +7,7 @@ and then saves into a  file
 
 from sys import argv
 
+
 savejs = __import__('5-save_to_json_file').save_to_json_file
 loadjs = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
