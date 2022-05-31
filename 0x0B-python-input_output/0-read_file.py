7ffe7c9ce3b025ea52010e9eb38358970daa5a90
declannoble <declanjnoble@hotmail.com>
2022-05-31 01:14:21 -0700

    changed open

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ module to read a text file and print to stdout
 
 def read_file(filename=""):
     """function that reads and prints files"""
-    with open("my_file_0.txt", "r", encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         print(f.read())
