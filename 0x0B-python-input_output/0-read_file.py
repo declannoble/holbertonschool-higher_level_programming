5ebcda808f2750f658e79a920cd0745367bd0515
declannoble <declanjnoble@hotmail.com>
2022-05-31 01:15:32 -0700

    added newline

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ module to read a text file and print to stdout
 def read_file(filename=""):
     """function that reads and prints files"""
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
