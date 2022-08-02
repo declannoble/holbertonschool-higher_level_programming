8ae61f677cb9e6020a4a83c37650ea6e945699e9
declannoble <declanjnoble@hotmail.com>
2022-08-01 20:46:31 -0700

    build: change conditions

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -11,8 +11,8 @@ module.exports = class Square extends Rectangle {
     if (c === undefined) {
       this.print();
     } else {
-      for (let i = 0; i < this.height; i++) {
-        console.log('C'.repeat(this.width));
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.height));
       }
     }
   }
