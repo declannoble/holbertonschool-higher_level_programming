28a0e1398c275bb608c1447f880cafaa9e0689de
declannoble <declanjnoble@hotmail.com>
2022-08-03 18:05:50 +1000

    fix: add connect call

--- 0x0F-python_object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python_object_relational_mapping/2-my_filter_states.py
@@ -7,12 +7,12 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    dbConnection = MySQLdb(host="localhost", 
-                        port=3306, 
-                        user=sys.argv[1], 
-                        passwd=sys.argv[2],
-                        db=sys.argv[3],
-                        charset="utf8")
+    dbConnection = MySQLdb.connect(host="localhost", 
+                                port=3306, 
+                                user=sys.argv[1], 
+                                passwd=sys.argv[2],
+                                db=sys.argv[3],
+                                charset="utf8")
 
     dbCursor = dbConnection.cursor()
     dbCursor.execute(f"SELECT * FROM states where states.name = {sys.argv[4]} ORDER BY id ASC")
