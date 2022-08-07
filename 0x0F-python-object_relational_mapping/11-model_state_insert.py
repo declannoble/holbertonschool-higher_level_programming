5864263235e0a6ea629c77316e920cff6e992853
declannoble <declanjnoble@hotmail.com>
2022-08-06 19:15:38 -0700

    build: initial commit

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" script that adds the State object “Louisiana” to the database
+ hbtn_0e_6_usa """
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session, sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state6 = State(name="Louisiana")
+    session.add(state6)
+    session.commit()
+    print(f"{state6.id}")
+    session.close()
