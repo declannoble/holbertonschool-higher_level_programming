ee2b055beaf500566ad79997be384e69d02e50bd
declannoble <declanjnoble@hotmail.com>
2022-08-06 18:19:08 -0700

    fix: pycodestyle/shortened variable name

--- 0x0F-python_object_relational_mapping/1-filter_states.py
+++ 0x0F-python_object_relational_mapping/1-filter_states.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""
-script to list all states starting with aa capital N
-"""
-
+""" script that lists all states with a name starting with N from
+the database hbtn_0e_0_usa """
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    connection = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                                 passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-    kerser = connection.cursor()
-    kerser.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    rowContent = kerser.fetchall()
-    for rows in rowContent:
-        print (rows)
-
-    kerser.close()
-    connection.close()
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
