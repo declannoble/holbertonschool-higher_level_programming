b9e9c8440c7fbf56ff4d1a4fd03d92575c634442
declannoble <declanjnoble@hotmail.com>
2022-08-06 18:14:17 -0700

    build: initial commit

--- 0x0F-python_object_relational_mapping/0-select_states.py
+++ 0x0F-python_object_relational_mapping/0-select_states.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-'''
-This module is a basic interaction with
-a MYSQL DB
-'''
+""" script that lists all states from the database hbtn_0e_0_usa """
 
-import MySQLdb
+
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-   connection = MySQLdb.connect(host="localhost",
-                                      port=3306,
-                                      user=sys.argv[1],
-                                      passwd=sys.argv[2],
-                                      db=sys.argv[3])
-   kerser = connection.cursor()
-   kerser.execute('''
-                     SELECT * FROM states
-                     ORDER BY id ASC
-                     ''')
-   query_rows = kerser.fetchall()
-   for row in query_rows:
-      print(row)
-   kerser.close()
-   connection.close()
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
