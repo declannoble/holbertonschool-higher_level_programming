d7f11113e765ded2ebff46faded3736166473700
declannoble <declanjnoble@hotmail.com>
2022-08-17 12:04:49 +1000

    build: initial commit

---
+++ 0x10-python_network/0-body_size.sh
@@ -0,0 +1,4 @@
+#!/bin/bash                                                                                                                  
+# script to take in url, send a request and display body size of response                                                    
+
+curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
