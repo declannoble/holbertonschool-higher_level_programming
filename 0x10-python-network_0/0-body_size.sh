29f4dcc1752e23df4f96ba05ec32db9a24cc8a1b
declannoble <declanjnoble@hotmail.com>
2022-08-18 10:27:52 +1000

    fix: remove line

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash                                                                                                                  
 # script to take in url, send a request and display body size of response                                                    
-
 curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
