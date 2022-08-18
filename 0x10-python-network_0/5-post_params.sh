bdb62199423a702b237aeec199de26e844586113
declannoble <declanjnoble@hotmail.com>
2022-08-17 18:04:26 -0700

    refactor: typo

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script to take in url as arg, send POST request and display response body
-curl -s	"$1" -X	POST -d	"email=test@gmail.com&subject=I will always be there for PLD"
+curl -s	"$1" -X	POST -d	"email=test@gmail.com&subject=I will always be here for PLD"
