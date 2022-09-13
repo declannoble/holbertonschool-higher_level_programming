0121784489a8c2a743634a8a477e06e101710d34
declannoble <declanjnoble@hotmail.com>
2022-09-13 06:54:58 +0000

    fix: semistandard

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const axios = require('axios').default;
+const URL = process.argv[2];
+const filePath = process.argv[3]
+
+axios.get(URL).then((response) => {
+    fs.writeFile(filePath, data, 'utf-8', function (err) {
+	if (err) {
+	    console.log(err);
+	}
+    });
+});
