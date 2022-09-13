aea99603382e1bb993fe017a35e600c20e60dd20
declannoble <declanjnoble@hotmail.com>
2022-09-13 06:59:24 +0000

    fix: semistandard

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,12 +3,12 @@
 const fs = require('fs');
 const axios = require('axios').default;
 const URL = process.argv[2];
-const filePath = process.argv[3]
+const filePath = process.argv[3];
 
 axios.get(URL).then((response) => {
-    fs.writeFile(filePath, response.data, 'utf-8', function (err) {
-	if (err) {
+  fs.writeFile(filePath, response.data, 'utf-8', function (err) {
+    if (err) {
 	    console.log(err);
-	}
-    });
+    }
+  });
 });
